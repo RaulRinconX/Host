aee8030eab8b23f1ee73e5bf8c3580af76efc646
Raul Santiago Rincon <92411789+RaulRinconX@users.noreply.github.com>
2025-02-28 09:32:12 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -15,20 +15,25 @@ fi
 # Queremos saber el propietario del archivo
 
 owner=$(stat -c "%U" "$archivo")
-permisos=$(ls -l "$archivo" | awk '{print $1}' | cut -c2-4)
-
-echo "$owner, $permisos"
-
-
-# Queremos saber la lista de usuarios con acceso
-
 grupo=$(stat -c "%G" "$archivo")
-permisos_grupo=$(ls -l "$archivo" | awk '{print $1}' | cut -c5-7)
+perm=$(ls -l "$archivo" | awk '{print $1}')
 
-for user in $(getent group "$grupo" | cut -d: -f4 | tr ',' ' '); do
-	echo "$user, $permisos_grupo"
+
+perm_owner=$(echo "$perm" | cut -c2-4)
+perm_grupo=$(echo "$perm" | cut -c5-7)
+perm_otros=$(echo "$perm" | cut -c8-10)
+
+usuarios=$(getent passwd | awk -F: '$3 >= 1000 {print $1}')
+
+for usuario in $usuarios; do
+	if [ "$usuario" == "$owner" ]; then
+		echo "$usuario, $perm_owner"
+	elif id -nG "$usuario" | grep -qw "$grupo"; then
+		echo "$usuario, $perm_grupo"
+	else
+		echo "$usuario, $perm_otros"
+	fi
 done
-
 
 
 
